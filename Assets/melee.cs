b2b803d0c8e39c9f327aa2b8cec1064ab04989d9
MaxwellSagus <46203503+MaxwellSagus@users.noreply.github.com>
2018-12-29 13:57:06 -0600

    Trying?

---
+++ Assets/melee.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class melee : MonoBehaviour {
+    Animator manim;
+	// Use this for initialization
+	void Start ()
+    {
+        manim = GameObject.FindWithTag("Player").GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetMouseButtonDown(0))
+        {
+            manim.SetTrigger("hit");
+        }
+	}
+}
---
+++ Assets/weapon_pickup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon_pickup : MonoBehaviour {
+
+    public GameObject hand;
+    Vector3 zero = new Vector3(0, 0, 0);
+    Vector3 SwordRotation = new Vector3(0, 35, 0);
+    
+    
+	// Use this for initialization
+	void Start () {
+
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag == "holdable")
+        {
+            collider.transform.position = hand.transform.position;
+            collider.transform.rotation = Quaternion.FromToRotation(zero,SwordRotation);
+
+            collider.transform.parent = hand.transform;
+        }
+    }
+}
